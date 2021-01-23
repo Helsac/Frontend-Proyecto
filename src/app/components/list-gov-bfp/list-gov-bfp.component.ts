5561b7dbcaec47341695f15b71b2a5c8dc575d4e
Jonathan <jonathanvillegas202000@gmail.com>
2021-01-22 20:29:52 -0500

    Se añadió el ordenamiento por columna y se corrigieron algunos errores

--- src/app/components/list-gov-bfp/list-gov-bfp.component.ts
+++ src/app/components/list-gov-bfp/list-gov-bfp.component.ts
@@ -16,7 +16,11 @@ import { CommunicationService } from 'src/app/services/communication.service';
 export class ListGovBFPComponent implements OnInit {
 
   constructor(private govBFPService : GovernmentBFPService, private communicationService : CommunicationService) { }
+  tableHeaders : string[] = ["Year of balance" , "Actual/Revised/Estimates", "Category"
+  , "Item" , "Amount( S$ million)", "Percent of gdp"];
 
+  tableHeaderR : string[] = ["yearOfBalance", "state", "category", "item", "amount", 
+  "percentOfGdp"];
   govBFPs! : GovernmentBFP[]
   page : number
   previousPage : number
@@ -25,12 +29,16 @@ export class ListGovBFPComponent implements OnInit {
   showPagination : boolean = false
   loadedD : boolean = false
   selectedRow : number
+  lastSort: number
+  decre : boolean
+
   ngOnInit(): void {
     this.loadData()
     this.communicationService.reloadObservable.subscribe(response =>{
       this.loadData();
       console.log("Actualizando");
       this.setClickedRow(-1);
+      
     });
 
     this.page = 1
@@ -38,6 +46,8 @@ export class ListGovBFPComponent implements OnInit {
     this.showPagination = true
     this.pageSize = 15
     this.selectedRow = -1
+    this.decre = false
+    this.lastSort = -1
   }
 
 
@@ -45,9 +55,13 @@ export class ListGovBFPComponent implements OnInit {
     this.govBFPService.listGovBFP().subscribe(
       
       govBFPs => {this.govBFPs = govBFPs;
-        console.log(this.govBFPs)
-        console.log(this.loadedD)
         this.loadedD = true
+        if(this.lastSort != -1){
+          this.decre = !this.decre
+          console.log(this.lastSort)
+          this.sortT(this.lastSort);
+          console.log(this.govBFPs)
+        }
       }
  
     );
@@ -67,5 +81,22 @@ export class ListGovBFPComponent implements OnInit {
   formatNumber(num : number){
     return Intl.NumberFormat('en-us', {minimumFractionDigits: 2}).format(num)
 
+  }
+
+  sortT(act : number){
+    console.log("Sorting")
+    if(act == this.lastSort){
+      this.decre = !this.decre
+    }
+    else{
+      this.decre = false;
+    }
+    if(this.decre == true){
+      this.govBFPs.sort((a, b) => (a[this.tableHeaderR[act]] < b[this.tableHeaderR[act]] ? 1 : a[this.tableHeaderR[act]] > b[this.tableHeaderR[act]] ? -1 : 0))
+    }
+    else{
+      this.govBFPs.sort((a, b) => (a[this.tableHeaderR[act]] > b[this.tableHeaderR[act]] ? 1 : a[this.tableHeaderR[act]] < b[this.tableHeaderR[act]] ? -1 : 0))
+    }
+    this.lastSort = act;
   }
 }
